b49b1c7456f06f32473f80b7ff9d9385d4f90b0c
Exequiel José Mendoza <exejm92@gmail.com>
2025-05-23 21:22:28 -0300

    se modificó código Ejercicio-6

--- Ejercicio-6/Ejercicio-6.js
+++ Ejercicio-6/Ejercicio-6.js
@@ -5,6 +5,11 @@ const pApellido = document.getElementById("pApellido");
 const pEdad = document.getElementById("pEdad");
 const pAltura = document.getElementById("pAltura");
 const pCorreo = document.getElementById("pCorreo");
+const sNombre = document.getElementById("sNombre");
+const sApellido = document.getElementById("sApellido");
+const sEdad = document.getElementById("sEdad");
+const sAltura = document.getElementById("sAltura");
+const sCorreo = document.getElementById("sCorreo");
 
 boton.addEventListener("click", () => {
   const nombre = document.getElementById("nombre").value;
@@ -14,47 +19,58 @@ boton.addEventListener("click", () => {
   const correoElectronico = document.getElementById("correoElectronico").value;
 
   if (nombre.length > 0 && nombre.length < 51) {
+    sNombre.style.color = "green";
     pNombre.style.color = "green";
-    pNombre.textContent = `Nombre: ${nombre}`;
+    pNombre.textContent = ` ${nombre}`;
   } else {
     pNombre.style.color = "red";
-    pNombre.textContent = `Nombre: Campo vacío`;
+    sNombre.style.color = "red";
+    pNombre.textContent = ` Campo vacío`;
   }
 
   if (apellido.length > 0 && apellido.length < 51) {
+    sApellido.style.color = "green";
     pApellido.style.color = "green";
-    pApellido.textContent = `Apellido: ${apellido}`;
+    pApellido.textContent = ` ${apellido}`;
   } else {
+    sApellido.style.color = "red";
     pApellido.style.color = "red";
-    pApellido.textContent = `Apellido: Campo vacío`;
+    pApellido.textContent = ` Campo vacío`;
   }
 
   if (edad > 17 && edad < 120) {
+    sEdad.style.color = "green";
     pEdad.style.color = "green";
-    pEdad.textContent = `Edad: ${edad} años`;
+    pEdad.textContent = ` ${edad} años`;
   } else {
     if (edad > 0 && edad < 18) {
-      pEdad.style.color = "red";
-      pEdad.textContent = `Edad: ${edad} años ¡ES MENOR DE EDAD!`;
+      sEdad.style.color = "green";
+      pEdad.style.color = "green";
+      pEdad.textContent = ` ${edad} años ¡MENOR DE EDAD!`;
     } else {
+      sEdad.style.color = "red";
       pEdad.style.color = "red";
-      pEdad.textContent = `Edad: Valor ingresado no válido`;
+      pEdad.textContent = `Campo vacío o valor ingresado no válido`;
     }
   }
 
   if (altura >= 1 && altura < 230) {
+    sAltura.style.color = "green";
     pAltura.style.color = "green";
-    pAltura.textContent = `Altura: ${altura} cm.`;
+    pAltura.textContent = `${altura} cm.`;
   } else {
+    sAltura.style.color = "red";
     pAltura.style.color = "red";
-    pAltura.textContent = `Altura: Valor ingresado no válido`;
+    pAltura.textContent = `Campo vacío o valor ingresado no válido`;
   }
 
   if (correoElectronico.includes("@")) {
+    sCorreo.style.color = "green";
     pCorreo.style.color = "green";
-    pCorreo.textContent = `Correo electrónico: ${correoElectronico}`;
+    pCorreo.textContent = ` ${correoElectronico}`;
   } else {
+    sCorreo.style.color = "red";
     pCorreo.style.color = "red";
-    pCorreo.textContent = `Correo electrónico: ¡ERROR! Ingrese "@"`;
+    pCorreo.textContent = `Campo vacío o no se ingresó "@"`;
   }
 });
